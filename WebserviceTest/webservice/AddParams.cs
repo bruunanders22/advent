164fa519408f8e1d885bc7a24ea3cb67c15c3265
Anders <anders>
2022-04-07 14:11:36 +0200

    ...

--- WebserviceTest/webservice/AddParams.cs
+++ WebserviceTest/webservice/AddParams.cs
@@ -1,30 +1,39 @@
- public class AddParams
- {
-     public double X { get; set; }
 
-     public double Y { get; set; }
+public class AddOp
+{
+    public eval(double x, double y)
+    {
+        return x+y;
+    }
+}
 
-     public string ToJson()
-     {
-         Result result = new Result(X + Y);
-         string json  = System.Text.Json.JsonSerializer.Serialize<Result>(result);
-         return json;
-     }
+public class AddParams
+{
+    public double X { get; set; }
 
-     public static bool TryParse(string? value, IFormatProvider? provider, out AddParams addParam)
-     {
-         // Format is "(12.3,10.1)"
-         var trimmedValue = value?.TrimStart('(').TrimEnd(')');
-         var segments = trimmedValue?.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
-         if (segments?.Length == 2
-             && double.TryParse(segments[0], out var x)
-             && double.TryParse(segments[1], out var y))
-         {
-             addParam = new AddParams { X = x, Y = y };
-             return true;
-         }
+    public double Y { get; set; }
 
-         addParam = new AddParams();
-         return false;
-     }
- }
+    public string ToJson()
+    {
+        Result result = new Result(X + Y);
+        string json  = System.Text.Json.JsonSerializer.Serialize<Result>(result);
+        return json;
+    }
+
+    public static bool TryParse(string? value, IFormatProvider? provider, out AddParams addParam)
+    {
+        // Format is "(12.3,10.1)"
+        var trimmedValue = value?.TrimStart('(').TrimEnd(')');
+        var segments = trimmedValue?.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        if (segments?.Length == 2
+         && double.TryParse(segments[0], out var x)
+         && double.TryParse(segments[1], out var y))
+        {
+            addParam = new AddParams { X = x, Y = y };
+            return true;
+        }
+
+        addParam = new AddParams();
+        return false;
+    }
+}
